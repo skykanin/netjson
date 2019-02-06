398e50029f00b59743ba4e0459917e955d75bfbc
skykanin <3789764+skykanin@users.noreply.github.com>
2019-02-06 17:52:41 +0100

    Fixed license notice commenting

--- src/netjson/core.clj
+++ src/netjson/core.clj
@@ -1,16 +1,16 @@
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (ns netjson.core
   (:require [cheshire.core :refer [parse-string]])
   (:require [clojure.java.io :as io]))
@@ -26,7 +26,7 @@
       (assoc :statistics
              (select-keys (:statistics data)
                           [:collisions :rx_frame_errors :rx_packets :tx_packets]))))
-                            
+
 (defn collision-stat [json]
   (->> json
        (:interfaces)
