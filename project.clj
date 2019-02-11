40325ca1c26e969a23ed33011701c3f29665d732
skykanin <3789764+skykanin@users.noreply.github.com>
2019-02-11 14:59:05 +0100

    Core now manages server

--- project.clj
+++ project.clj
@@ -4,5 +4,14 @@
   :license {:name "GNU GPLv3+"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.8.1"]]
-  :resource-paths ["resources/netjson/"])
+                 [cheshire "5.8.1"]
+                 [org.clojure/clojurescript "1.10.516"]
+                 [http-kit "2.3.0"]
+                 [compojure "1.6.1"]]
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.7"]]
+                   :cljsbuild {:builds [{:source-paths ["src"]
+                                         :compiler {:output-to "target/classes/public/app.js"
+                                                    :optimizations :whitespace
+                                                    :pretty-print true}}]}}}
+  :resource-paths ["resources" "resources/netjson/"]
+  :main netjson.core)
--- src/netjson/core.clj
+++ src/netjson/core.clj
@@ -12,26 +12,19 @@
 ; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (ns netjson.core
-  (:require [cheshire.core :refer [parse-string]])
-  (:require [clojure.java.io :as io]))
+  (:require [org.httpkit.server :refer [run-server]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [resources]]))
 
-(def data (io/resource "netjson.json"))
+(defn index [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello HTTP via Compojure!"})
 
-(defn get-data [path]
-  (parse-string (slurp path) keyword))
+(defroutes app
+           (resources "/")
+           (GET "/" [] index))
 
-(defn sub-stat [data]
-  (-> data
-      (select-keys [:name :uptime])
-      (assoc :statistics
-             (select-keys (:statistics data)
-                          [:collisions :rx_frame_errors :rx_packets :tx_packets]))))
-
-(defn collision-stat [json]
-  (->> json
-       (:interfaces)
-       (filter #(pos? (:collisions (:statistics %))))
-       (map sub-stat)
-       (vec)))
-
-(collision-stat (get-data data))
+(defn -main [& args]
+  (run-server app {:port 8080})
+  (println (format "Started server on localhost:%d" 8080)))
